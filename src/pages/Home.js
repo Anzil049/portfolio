04308a719885e2eb230e5173f948c40cbf1eff20
anzil049 <anzil049@gmail.com>
2026-01-15 15:19:18 +0530

    home fix

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <>
+      <h1>Anzil K</h1>
+      <h3>MERN Stack Developer</h3>
+      <p>
+        Passionate about building scalable, user-friendly web applications
+        using modern technologies.
+      </p>
+    </>
+  );
+}
+
+export default Home;
