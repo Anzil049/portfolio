6f7b11f641ff99f992850c2d17c08e5f1ad57db9
anzil049 <anzil049@gmail.com>
2026-01-15 15:19:53 +0530

    About fix

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <>
+      <h2>About Me</h2>
+      <p>
+        I am a MERN Stack Developer with hands-on experience in building
+        full-stack applications using React, Node.js, Express, and MongoDB.
+      </p>
+    </>
+  );
+}
+
+export default About;
